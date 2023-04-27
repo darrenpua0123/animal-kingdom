f98cf3403eea18e9d64123b3946a0f63edd60d87
Darren Pua <darrenpua0123@gmail.com>
2023-04-28 02:41:00 +0800

    Added Login function

--- Animal Kingdom Brawl!/Assets/Scripts/LoginScript.cs
+++ Animal Kingdom Brawl!/Assets/Scripts/LoginScript.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Auth;
+using Unity.VisualScripting;
 
 public class LoginScript : MonoBehaviour
 {
     public TextMeshProUGUI promptText;
-    private string loginString = "Welcome back! Please login!";
-    private string registerString = "Welcome! Please sign up!";
-    private string requiredInput = "All fields are required!";
+    private readonly string loginString = "Welcome back! Please login!";
+    private readonly string registerString = "Welcome! Please sign up!";
+    private readonly string requiredInput = "All fields are required!";
+    private readonly string userNotExist = "User does not exist!";
+    private readonly string invalidEmailFormat = "Invalid email address format!";
+    private readonly string wrongPassword = "Wrong password!";
+
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser user;
 
     [Header("Login")]
     public GameObject loginPanel;
@@ -24,6 +35,24 @@ public class LoginScript : MonoBehaviour
 
     private string registerGender = "male";
 
+    void Awake()
+    {
+        // Validate all necessary dependencies for Firebase are presented 
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                InitialiseFirebase();
+            }
+            else
+            {
+                // Dependencies missing
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
     void Start()
     {
 
@@ -32,6 +61,11 @@ public class LoginScript : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void InitialiseFirebase() 
+    {
+        auth = FirebaseAuth.DefaultInstance;
     }
 
     private void SetPromptMessage(string message)
@@ -48,13 +82,21 @@ public class LoginScript : MonoBehaviour
         registerPanel.SetActive(true);
     }
 
-    public void LoginUser() {
+    public void LoginButton() {
 
-        if (LoginFieldIsEmpty()) {
+        if (LoginFieldIsEmpty())
+        {
             SetPromptMessage(requiredInput);
+            return;
+        }
+        else 
+        { 
+            loginUsername = loginUsernameInput.text;
+            loginEmail = loginEmailInput.text;
+            loginPassword = loginPasswordInput.text;
         }
 
-        ResetLoginInputField();
+        StartCoroutine(Login(loginEmail, loginPassword));
         //SceneManager.LoadScene("MainMenuScene");
     }
 
@@ -72,6 +114,42 @@ public class LoginScript : MonoBehaviour
         loginUsernameInput.text = null;
         loginEmailInput.text = null;
         loginPasswordInput.text = null;
+    }
+
+    private IEnumerator Login(string email, string password)
+    {
+        var loginTask = auth.SignInWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(predicate: () => loginTask.IsCompleted);
+
+        if (loginTask.Exception != null)
+        {
+            // All login error handling
+            Debug.LogWarning(message: $"Failed to register task with {loginTask.Exception}");
+            FirebaseException firebaseExp = loginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError) firebaseExp.ErrorCode;
+
+            switch (errorCode)
+            {
+                case AuthError.InvalidEmail:
+                    SetPromptMessage(invalidEmailFormat);
+                    break;
+
+                case AuthError.UserNotFound:
+                    SetPromptMessage(userNotExist);
+                    break;
+
+                case AuthError.WrongPassword:
+                    SetPromptMessage(wrongPassword);
+                    break;
+            }
+        }
+        else
+        {
+            user = loginTask.Result;
+            Debug.Log($"User signed in: {user.DisplayName} and {user.Email}");
+        }
+
+        ResetLoginInputField();
     }
 
     // REGISTER
@@ -97,13 +175,15 @@ public class LoginScript : MonoBehaviour
         registerPanel.SetActive(false);
     }
 
-    public void RegisterUser()
+    public void RegisterButton()
     {
         if (RegisterFieldIsEmpty())
         {
             SetPromptMessage(requiredInput);
+            return;
         }
 
+        // https://www.youtube.com/watch?v=NsAUEyA2TRo&t=553s 9mins
         ResetRegisterInputField();
         //SceneManager.LoadScene("MainMenuScene");
     }
@@ -130,4 +210,8 @@ public class LoginScript : MonoBehaviour
         registerPasswordInput.text = null;
         registerRepasswordInput.text = null;
     }
+
+    //private IEnumerator Register() {
+    // TODO: Add in register user to Firebase, refer yooutube
+    //}
 }
