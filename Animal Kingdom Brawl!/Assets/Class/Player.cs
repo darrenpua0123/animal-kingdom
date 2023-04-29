c7e9d16e40f2c7f6668014bd47298741f528fbe1
Darren Pua <darrenpua0123@gmail.com>
2023-04-30 03:20:09 +0800

    Added basic register user function
    However, checking for username taken error is still not implemented.

---
+++ Animal Kingdom Brawl!/Assets/Class/Player.cs
@@ -0,0 +1,17 @@
+﻿
+public class Player
+{
+    public string id { get; set; }
+    public string username { get; set; }    
+    //public int age { get; set; }
+    //public string gender { get; set; }
+    //public string email { get; set; }
+    public string[] unlockedAnimalHeroes;
+
+    public Player(string id, string username)
+    {
+        this.id = id;
+        this.username = username;
+    }
+
+}
--- Animal Kingdom Brawl!/Assets/Scripts/LoginScript.cs
+++ Animal Kingdom Brawl!/Assets/Scripts/LoginScript.cs
@@ -6,16 +6,24 @@ using UnityEngine.SceneManagement;
 using Firebase;
 using Firebase.Auth;
 using Unity.VisualScripting;
+using Firebase.Database;
 
 public class LoginScript : MonoBehaviour
 {
     public TextMeshProUGUI promptText;
     private readonly string loginString = "Welcome back! Please login!";
     private readonly string registerString = "Welcome! Please sign up!";
+    private readonly string userRegistered = "You are now registered, yay! Please login!";
     private readonly string requiredInput = "All fields are required!";
     private readonly string userNotExist = "User does not exist!";
     private readonly string invalidEmailFormat = "Invalid email address format!";
     private readonly string wrongPassword = "Wrong password!";
+
+    private readonly string usernameTooShort = "Username too short! Must be at least 4 characters!";
+    private readonly string ageInvalidFormat = "Age is invalid! The age range is between 1 to 120!";
+    private readonly string emailTaken = "Email already taken!";
+    private readonly string passwordTooShort = "Weak password! Password must be at least 6 characters!";
+    private readonly string passwordMismatch = "The repeated password is incorrect!";
 
     [Header("Firebase")]
     public DependencyStatus dependencyStatus;
@@ -32,8 +40,12 @@ public class LoginScript : MonoBehaviour
     [Header("Register")]
     public GameObject registerPanel;
     public TMP_InputField registerUsernameInput, registerAgeInput, registerEmailInput, registerPasswordInput, registerRepasswordInput;
-
+    private string registerUsername;
+    private string registerAge;
     private string registerGender = "male";
+    private string registerEmail;
+    private string registerPassword;
+    private string registerRepassword;
 
     void Awake()
     {
@@ -63,7 +75,7 @@ public class LoginScript : MonoBehaviour
 
     }
 
-    private void InitialiseFirebase() 
+    private void InitialiseFirebase()
     {
         auth = FirebaseAuth.DefaultInstance;
     }
@@ -82,22 +94,20 @@ public class LoginScript : MonoBehaviour
         registerPanel.SetActive(true);
     }
 
-    public void LoginButton() {
+    public void LoginButton()
+    {
+        loginUsername = loginUsernameInput.text;
+        loginEmail = loginEmailInput.text;
+        loginPassword = loginPasswordInput.text;
 
+        // One or more Login fields are empty
         if (LoginFieldIsEmpty())
         {
             SetPromptMessage(requiredInput);
             return;
         }
-        else 
-        { 
-            loginUsername = loginUsernameInput.text;
-            loginEmail = loginEmailInput.text;
-            loginPassword = loginPasswordInput.text;
-        }
 
         StartCoroutine(Login(loginEmail, loginPassword));
-        //SceneManager.LoadScene("MainMenuScene");
     }
 
     private bool LoginFieldIsEmpty()
@@ -124,9 +134,9 @@ public class LoginScript : MonoBehaviour
         if (loginTask.Exception != null)
         {
             // All login error handling
-            Debug.LogWarning(message: $"Failed to register task with {loginTask.Exception}");
+            Debug.LogWarning(message: $"Failed to login task with {loginTask.Exception}");
             FirebaseException firebaseExp = loginTask.Exception.GetBaseException() as FirebaseException;
-            AuthError errorCode = (AuthError) firebaseExp.ErrorCode;
+            AuthError errorCode = (AuthError)firebaseExp.ErrorCode;
 
             switch (errorCode)
             {
@@ -145,8 +155,11 @@ public class LoginScript : MonoBehaviour
         }
         else
         {
+            // TODO: User loggged in, perform logic here
             user = loginTask.Result;
-            Debug.Log($"User signed in: {user.DisplayName} and {user.Email}");
+            SceneManager.LoadScene("MainMenuScene");
+
+            Debug.Log($"User signed in: {user.UserId}, {user.DisplayName}, and {user.Email}");
         }
 
         ResetLoginInputField();
@@ -177,15 +190,46 @@ public class LoginScript : MonoBehaviour
 
     public void RegisterButton()
     {
+        registerUsername = registerUsernameInput.text;
+        registerAge = registerAgeInput.text;
+        // var registerGender set during OnGenderChanged;
+        registerEmail = registerEmailInput.text;
+        registerPassword = registerPasswordInput.text;
+        registerRepassword = registerRepasswordInput.text;
+
+        // One or more fields are empty
         if (RegisterFieldIsEmpty())
         {
             SetPromptMessage(requiredInput);
             return;
         }
+        // Username too short or long
+        else if (UsernameTooShortOrLong())
+        {
+            SetPromptMessage(usernameTooShort);
+            return;
+        }
 
-        // https://www.youtube.com/watch?v=NsAUEyA2TRo&t=553s 9mins
-        ResetRegisterInputField();
-        //SceneManager.LoadScene("MainMenuScene");
+        // TODO: MISSING USERNAME TAKEN
+        //else if ()
+        //{ 
+        
+        //}
+
+        // Age >= 120, or  <= 0 
+        else if (AgeInvalid())
+        {
+            SetPromptMessage(ageInvalidFormat);
+            return;
+        }
+        // Password mismatch
+        else if (PasswordIsMismatch()) 
+        {
+            SetPromptMessage(passwordMismatch);
+            return;
+        }
+
+        StartCoroutine(Register(registerUsername, registerEmail, registerPassword));
     }
 
     private bool RegisterFieldIsEmpty()
@@ -197,21 +241,107 @@ public class LoginScript : MonoBehaviour
         string repasswordText = registerRepasswordInput.text;
 
         return (
-            string.IsNullOrEmpty(userText) || string.IsNullOrEmpty(ageText) || string.IsNullOrEmpty(emailText) || 
+            string.IsNullOrEmpty(userText) || string.IsNullOrEmpty(ageText) || string.IsNullOrEmpty(emailText) ||
             string.IsNullOrEmpty(passwordText) || string.IsNullOrEmpty(repasswordText)
         );
+    }
+
+    private bool UsernameTooShortOrLong() 
+    {
+        string username = registerUsernameInput.text;
+
+        return (username.Length < 4 || username.Length >= 21);
+    }
+
+    private bool AgeInvalid() 
+    {
+        int age = int.Parse(registerAgeInput.text);
+
+        return (age <= 0 || age >= 121);
+    }
+
+    private bool PasswordIsMismatch() 
+    { 
+        string password = registerPasswordInput.text;
+        string repassword = registerRepasswordInput.text;
+
+        return (!password.Equals(repassword));
     }
 
     private void ResetRegisterInputField()
     {
         registerUsernameInput.text = null;
-        registerAgeInput.text = null;  
-        registerEmailInput.text = null; 
+        registerAgeInput.text = null;
+        registerEmailInput.text = null;
         registerPasswordInput.text = null;
         registerRepasswordInput.text = null;
     }
 
-    //private IEnumerator Register() {
-    // TODO: Add in register user to Firebase, refer yooutube
-    //}
+    private IEnumerator Register(string username, string email, string password)
+    {
+        // Check username here
+
+        var registerTask = auth.CreateUserWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(predicate: () => registerTask.IsCompleted);
+
+        if (registerTask.Exception != null)
+        {
+            // All register error handling
+            Debug.LogWarning(message: $"Failed to register task with {registerTask.Exception}");
+            FirebaseException firebaseExp = registerTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)firebaseExp.ErrorCode;
+
+            switch (errorCode)
+            {
+                case AuthError.InvalidEmail:
+                    SetPromptMessage(invalidEmailFormat);
+                    break;
+
+                case AuthError.EmailAlreadyInUse:
+                    SetPromptMessage(emailTaken);
+                    break;
+
+                case AuthError.WeakPassword:
+                    SetPromptMessage(passwordTooShort);
+                    break;
+            }
+        }
+        else 
+        {
+            // User registered as FirebaseUser into FirebaseAuth
+            user = registerTask.Result;
+
+            if (user != null) 
+            {
+                // Creates new userprofile to FirebaseAuth with the username
+                UserProfile profile = new UserProfile{ DisplayName = username };
+
+                var profileUpdatetask = user.UpdateUserProfileAsync(profile);
+                yield return new WaitUntil(predicate: () => profileUpdatetask.IsCompleted);
+
+                if (profileUpdatetask.Exception != null)
+                {
+                    // All profile updates error handling
+                    Debug.LogWarning($"Failed to update username task with {profileUpdatetask.Exception}");
+                    FirebaseException firebaseExp = profileUpdatetask.Exception.GetBaseException() as FirebaseException;
+                    AuthError errorCode = (AuthError)firebaseExp.ErrorCode;
+
+                    //switch (errorCode)
+                    //{
+
+                    //}
+                }
+                else 
+                {   
+                    // User registered with DisplayName and as FirebaseAuth. All register logic goes here.
+                    ResetRegisterInputField();
+                    SetPromptMessage(userRegistered);
+
+                    loginPanel.SetActive(true);
+                    registerPanel.SetActive(false);
+                    Debug.Log($"User registered as: {user.DisplayName} and {user.Email}");
+                } 
+            }
+        }
+    }
 }
