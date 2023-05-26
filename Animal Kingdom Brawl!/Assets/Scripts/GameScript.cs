f20265a231e5b750c216a7621df75677d6f93a03
Darren Pua <darrenpua0123@gmail.com>
2023-05-26 17:38:20 +0800

    Added GameScene, required fixing

--- Animal Kingdom Brawl!/Assets/Scripts/GameScript.cs
+++ Animal Kingdom Brawl!/Assets/Scripts/GameScript.cs
@@ -7,27 +7,58 @@ public class GameScript : MonoBehaviour
 {
     public GameObject cardHolder;
     public GameObject cardPrefab;
+    public float cardSpacing = 0.5f;
     // TODO: Game Area
 
     // Start is called before the first frame update
     void Start()
     {
-        CenterPlayableCard();
+        DrawCard(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        CenterPlayableCard();
+
     }
 
-    public void DrawCard(int numberOfDraw) {
-        int count = 0;
+    public void PlayerEndTurn() 
+    {
+        DrawCard(3);
 
-        while (count < numberOfDraw) 
-        {    
+        //CenterPlayableCard();
+        CenterCards();
+    }
+
+    public void DrawCard(int numberOfDraw) 
+    {
+        for (int i = 0; i < numberOfDraw; i++)
+        {
             GameObject newCard = Instantiate(cardPrefab, cardHolder.transform);
-            count++;
+            newCard.transform.localPosition = Vector3.zero;
+            newCard.transform.localRotation = Quaternion.identity;
+        }   
+    }
+
+    private void CenterCards()
+    {
+        int cardCount = cardHolder.transform.childCount;
+        float totalWidth = (cardCount - 1) * cardSpacing;
+        float offsetX = totalWidth / 2f;
+        float offsetY = 0f;
+
+        if (cardCount > 1)
+        {
+            offsetY = ((cardCount - 1) * cardSpacing) / 2f;
+        }
+
+        Vector3 startingPosition = new Vector3(-offsetX, offsetY, -1f);
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            Transform cardTransform = cardHolder.transform.GetChild(i);
+            Vector3 newPosition = startingPosition + new Vector3(i * cardSpacing, -i * cardSpacing, 0f);
+            cardTransform.localPosition = newPosition;
         }
     }
 
@@ -69,7 +100,7 @@ public class GameScript : MonoBehaviour
             float xPos = startX + (cardWidth / 2f);
             float yPos = startY - (cardHeight / 2f);
 
-            Vector3 newPos = new Vector3(xPos, yPos, child.position.z);
+            Vector3 newPos = new Vector3(xPos, yPos, -1);
             child.position = newPos;
 
             startX += cardWidth;
